b20e4fda3da38f10bf090a13a7071c66c5210f82
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-10 14:22:38 +0900

    Docs: 기본 개발 세팅
    react, styled-component 설치, .gitignore, prettier, globalStyle ,폴더구조 설정,

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import GlobalStyle from './GlobalStyle';
+import React from 'react';
+import Search from './component/Search/Search';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Search />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Search/Search.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search = () => <div>search</div>;
+
+export default Search;
