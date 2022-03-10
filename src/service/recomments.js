f1b9943f3e65a6215d884fc3c9bbb705daf13db0
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-10 15:51:12 +0900

    Feat: 로컬 캐쉬 만료 시간 설정

--- src/service/recomments.js
+++ src/service/recomments.js
@@ -3,12 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 export const recommendsApi = createApi({
   reducerPath: 'recommends',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.clinicaltrialskorea.com'
+    baseUrl: 'https://api.clinicaltrialskorea.com',
+    keepUnusedDataFor: 30,
   }),
   endpoints: (builder) => ({
     getRecommends: builder.query({
       //query 추가필요
-      query: (searchWord) => `/api/v1/search-conditions/?name=${searchWord}`
+      query: (searchWord) => `/api/v1/search-conditions/?name=${searchWord}`,
+      keepUnusedDataFor: 5
     })
   })
 });
