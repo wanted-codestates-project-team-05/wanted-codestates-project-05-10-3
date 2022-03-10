778f75d8ff68c75392c8685fa5f1f808ad46a49f
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-10 23:34:25 +0900

    Feat: onChange 함수 debounce 기능 추가

--- src/component/Search/ControlledInput.js
+++ src/component/Search/ControlledInput.js
@@ -3,29 +3,47 @@ import { useGetRecommendsQuery } from '../../service/recomments';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 const ControlledInput = () => {
-  const [input, setInput] = useState();
-  const [localRecommends, setLocalRecommends] = useLocalStorage('localRecommends');
-  const { data, error, isLoading, isSuccess, isError } = useGetRecommendsQuery(input);
-  useEffect(() => {
-    setLocalRecommends([...localRecommends, { searchWord: input, recommends: data }]);
-    if(localRecommends.length > 5){
-      let newLocalRecommends = localRecommends;
-      newLocalRecommends.shift();
-      setLocalRecommends(newLocalRecommends);
-    }
-  }, [data]);
-  const handleChange = async (event) => {
-    setInput(event.target.value);
-  };
+    const [input, setInput] = useState();
+    const [localRecommends, setLocalRecommends] = useLocalStorage('localRecommends', []);
+    const [timer, setTimer] = useState(0);
+    const [searchWord, setSearchWord] = useState();
+    const { data, error, isLoading, isSuccess, isError } = useGetRecommendsQuery(searchWord);
+    useEffect(() => {
+      if (searchWord && data) {
+        if (localRecommends.length > 2) {
+          let newLocalRecommends = localRecommends;
+          newLocalRecommends.shift();
+          setLocalRecommends(newLocalRecommends);
+        }
+        setLocalRecommends([...localRecommends, {
+          searchWord: searchWord,
+          recommends: data
+        }]);
+      }
+    }, [data]);
 
-  return (
-    <>
-      <input type="text" onChange={handleChange} />
-      {isLoading && 'Loading...'}
-      {isError && error.message}
-      {isSuccess && data && data.map((recommend) => <h1 key={recommend.id}>{recommend.name}</h1>)}
-    </>
-  );
-};
+    const handleChange = async (event) => {
+      setInput(event.target.value);
+      if (timer) {
+        clearTimeout(timer);
+      }
+      const newTimer = setTimeout(() => {
+        if (event.target.value !== '') {
+          setSearchWord(event.target.value);
+        }
+      }, 800);
+      setTimer(newTimer);
+    };
+
+    return (
+      <>
+        <input type="text" onChange={handleChange} />
+        {isLoading && 'Loading...'}
+        {isError && error.message}
+        {isSuccess && data && input && data.map((recommend) => <h1 key={recommend.id}>{recommend.name}</h1>)}
+      </>
+    );
+  }
+;
 
 export default ControlledInput;
