b1b696c00a919bbbe48b3a09585d123b7aeed03d
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-10 18:07:42 +0900

    Feat: 로컬 스토리지 검색추천 데이터 최대 5개 저장

--- src/component/Search/ControlledInput.js
+++ src/component/Search/ControlledInput.js
@@ -7,7 +7,12 @@ const ControlledInput = () => {
   const [localRecommends, setLocalRecommends] = useLocalStorage('localRecommends');
   const { data, error, isLoading, isSuccess, isError } = useGetRecommendsQuery(input);
   useEffect(() => {
-    setLocalRecommends({ searchWord: input, recommends: data });
+    setLocalRecommends([...localRecommends, { searchWord: input, recommends: data }]);
+    if(localRecommends.length > 5){
+      let newLocalRecommends = localRecommends;
+      newLocalRecommends.shift();
+      setLocalRecommends(newLocalRecommends);
+    }
   }, [data]);
   const handleChange = async (event) => {
     setInput(event.target.value);
