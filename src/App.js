f08aed51807447122aa8a8751ee74c449756a46c
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-10 16:45:16 +0900

    Feat: input 태그에 검색 알고리즘 연결

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import GlobalStyle from './GlobalStyle';
 import React from 'react';
 import Search from './component/Search/Search';
-import { useGetRecommendsQuery } from './service/recomments';
+import ControlledInput from './component/Search/ControlledInput';
 
 function App() {
-  const { data, error, isLoading, isSuccess, isError } = useGetRecommendsQuery("암");
 
   return (
     <>
       <GlobalStyle />
       <h1>React Toolkit && RTK Query</h1>
-      {isLoading && "Loading..."}
-      {isError && error.message}
-      {isSuccess && data && data.map((recommend)=> <h1 key={recommend.id}>{recommend.name}</h1>)}
       <Search />
+      <ControlledInput />
     </>
   );
 }
--- src/component/Search/ControlledInput.js
+++ src/component/Search/ControlledInput.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useGetRecommendsQuery } from '../../service/recomments';
 
 const ControlledInput = () => {
-  const handleChange = (event) => {
-    console.log(event.target.value);
+  const [input, setInput] = useState();
+  const { data, error, isLoading, isSuccess, isError } = useGetRecommendsQuery(input);
+  const handleChange = async (event) => {
+    setInput(event.target.value);
   };
-  return <input type="text" onChange={handleChange} />;
+
+  return (
+    <>
+      <input type="text" onChange={handleChange} />
+      {isLoading && 'Loading...'}
+      {isError && error.message}
+      {isSuccess && data && data.map((recommend) => <h1 key={recommend.id}>{recommend.name}</h1>)}
+    </>
+  );
 };
 
 export default ControlledInput;
