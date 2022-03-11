5627ec5e0478bc75abc551d6769928371256f91d
곽윤구 <dbsrndi123@naver.com>
2022-03-11 09:28:22 +0900

    Remove: recommendList
    Design: input component 스타일 수정

--- src/stories/Input.js
+++ src/stories/Input.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import RecommendList from './RecommendList';
 
 export default function Input({ label, backgroundColor, buttonColor, setModal, modal = false }) {
   const [inputValue, setInputValue] = useState('');
@@ -11,6 +10,7 @@ export default function Input({ label, backgroundColor, buttonColor, setModal, m
 
   // api 데이터
   const [list] = useState(['소아암', '혈액암', '피부암', '췌장암']);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 키보드 이벤트 관리
   const handleKeyDown = (e) => {
@@ -22,8 +22,11 @@ export default function Input({ label, backgroundColor, buttonColor, setModal, m
       e.preventDefault();
       setIndex((prev) => (prev === null ? 0 : prev === list.length - 1 ? null : prev + 1));
     }
+  };
+
+  const handlKeyUp = (e) => {
     if (e.key === 'Enter') {
-      setInputValue(autoValue);
+      setInputValue(autoValue || inputValue);
       setAutoValue('');
       setFocus(false);
       e.currentTarget.blur();
@@ -41,7 +44,7 @@ export default function Input({ label, backgroundColor, buttonColor, setModal, m
   }, [list, index]);
 
   return (
-    <>
+    <Div>
       <Container onClick={handleClick} modal={modal}>
         <InputWrapper backgroundColor={backgroundColor} modal={modal}>
           {modal && (
@@ -73,8 +76,8 @@ export default function Input({ label, backgroundColor, buttonColor, setModal, m
             }}
             value={autoValue || inputValue}
             onFocus={() => setFocus(true)}
-            onBlur={() => setFocus(false)}
             onKeyDown={handleKeyDown}
+            onKeyUp={handlKeyUp}
           />
         </InputWrapper>
         {modal || <Button buttonColor={buttonColor}>검색</Button>}
@@ -82,9 +85,37 @@ export default function Input({ label, backgroundColor, buttonColor, setModal, m
 
       {/* 추천 검색어 목록 */}
       {focus && inputValue && (
-        <RecommendList setAutoValue={setAutoValue} setInputValue={setInputValue} setFocus={setFocus} index={index} />
+        <Recommend modal={modal}>
+          {isLoading && <LoadingText>검색 중...</LoadingText>}
+          {!isLoading && (
+            <>
+              <First>추천 검색어</First>
+              {list.map((item, idx) => {
+                return (
+                  <List
+                    key={idx}
+                    selected={index === idx}
+                    onClick={() => {
+                      setInputValue(item);
+                      setAutoValue('');
+                      setFocus(false);
+                    }}
+                  >
+                    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z"
+                        fill="#32383E"
+                      ></path>
+                    </svg>
+                    {item}
+                  </List>
+                );
+              })}
+            </>
+          )}
+        </Recommend>
       )}
-    </>
+    </Div>
   );
 }
 
@@ -95,6 +126,13 @@ Input.propTypes = {
   setModal: PropTypes.func.isRequired,
   modal: PropTypes.bool,
 };
+
+const Div = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 const Container = styled.div`
   position: relative;
@@ -175,4 +213,57 @@ const Button = styled.button`
   @media only screen and (max-width: 1024px) {
     display: none;
   }
+`;
+
+const Recommend = styled.ul`
+  box-sizing: border-box;
+  position: absolute;
+  ${(props) =>
+    props.modal
+      ? `
+    width: 100%;
+    top: 56px;
+  `
+      : `
+  width: 670px;
+  top: 80px;
+  `};
+  background-color: white;
+  border-radius: 20px;
+  list-style: none;
+  min-height: 40px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  @media only screen and (max-width: 1024px) {
+    ${(props) => (props.modal ? '' : 'display: none;')}
+  }
+`;
+
+const LoadingText = styled.span`
+  font-size: 13px;
+  padding-left: 20px;
+  color: gray;
+`;
+
+const First = styled.li`
+  padding: 5px 20px;
+  font-size: 13px;
+  color: gray;
+`;
+
+// 현재 인덱스가 요소의 인덱스가 같으면 배경색을 바꿔 선택되었음을 보여줌
+const List = styled.li`
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 20px;
+  font-weight: 700;
+  font-size: 15px;
+  background-color: ${(props) => (props.selected ? 'lightgray' : 'white')};
+  &:hover {
+    background-color: lightgray;
+  }
+  display: flex;
+  gap: 10px;
 `;
