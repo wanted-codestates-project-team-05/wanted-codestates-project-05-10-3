b69f43e1c1c2e50018b7a645f470e2dbe5339092
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-11 10:33:19 +0900

    Feat: 검색 ui 와 검색어 추천 알고리즘 연결

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import GlobalStyle from './GlobalStyle';
 import React from 'react';
 import Search from './component/Search/Search';
-import ControlledInput from './component/Search/ControlledInput';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>React Toolkit && RTK Query</h1>
       <Search />
-      <ControlledInput />
     </>
   );
 }
