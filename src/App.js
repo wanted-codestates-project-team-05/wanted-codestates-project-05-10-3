04ba7cd0292349c75f49e20f47d61f5cae2c4b44
kyk1211 <85268135+kyk1211@users.noreply.github.com>
2022-03-11 09:52:16 +0900

    [Client]/ Feat: input components (#6)
    * merge
    
    * storybook set
    
    * feat: 기본적인 대문 디자인 및 input 컴포넌트 기본 기능
    
    * storybook set
    
    * feat: 기본적인 대문 디자인 및 input 컴포넌트 기본 기능
    
    * Style: input 컴포넌트 반응형 css, input storybook 수정
    
    Feat: RecommendList 컴포넌트
    
    * Remove: recommendList
    
    Design: input component 스타일 수정
    
    * package-lock.json
    
    * storybook set
    
    * feat: 기본적인 대문 디자인 및 input 컴포넌트 기본 기능
    
    * storybook set
    
    * feat: 기본적인 대문 디자인 및 input 컴포넌트 기본 기능
    
    * npm i
    
    * module error fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Search from './component/Search/Search';
 import ControlledInput from './component/Search/ControlledInput';
 
 function App() {
-
   return (
     <>
       <GlobalStyle />
--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
-import { Blue } from '../../stories/Input.stories';
 import styled from 'styled-components';
+import Input from '../../stories/Input';
 
 const Search = () => {
+  const [modal, setModal] = useState(false);
+
+  if (modal)
+    return (
+      <ModalContainer>
+        <Input
+          label={'질환명을 입력해 주세요.'}
+          backgroundColor={'white'}
+          buttonColor={'#007be9'}
+          setModal={setModal}
+          modal={modal}
+        />
+      </ModalContainer>
+    );
+
   return (
     <Container>
       <TextWrapper>
         국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
       </TextWrapper>
-      <Blue />
+      <Input label={'질환명을 입력해 주세요.'} backgroundColor={'white'} buttonColor={'#007be9'} setModal={setModal} />
     </Container>
   );
 };
+
+const ModalContainer = styled.div`
+  box-sizing: border-box;
+  width: calc(100vw - (100vw - 100%));
+  height: 100vh;
+  background-color: white;
+  display: flex;
+`;
 
 const Container = styled.div`
   background-color: #cae9ff;
@@ -20,17 +43,22 @@ const Container = styled.div`
   align-items: center;
   padding-top: 60px;
   height: 100vh;
+  position: relative;
 `;
 
 const TextWrapper = styled.div`
   width: 670px;
-  font-size: 34px;
-  font-weight: 700;
-  height: 108.781px;
-  letter-spacing: -0.612px;
-  line-height: 54.4px;
+  font-size 34px;
+  font-weight 700;
+  letter-spacing -0.612px;
+  line-height 54.4px;
+  text-align center;
   margin-bottom: 20px;
-  text-align: center;
+  @media only screen and (max-width: 1024px) {
+    font-size: 20px;
+    letter-spacing: -0.36px;
+    line-height: 32px;
+  }
 `;
 
 export default Search;
