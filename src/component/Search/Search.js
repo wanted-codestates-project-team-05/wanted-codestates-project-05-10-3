eae92c7fbe261e02bb316b44fec81cc1da6264dc
kyk1211 <85268135+kyk1211@users.noreply.github.com>
2022-03-11 09:15:20 +0900

    [Client]/ #2 / feat: 스토리북 기본 설정, input 컴포넌트, 기본 대문 디자인 (#3)
    * storybook set
    
    * feat: 기본적인 대문 디자인 및 input 컴포넌트 기본 기능

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Blue } from '../../stories/Input.stories';
+import styled from 'styled-components';
 
-const Search = () => <div>search</div>;
+const Search = () => {
+  return (
+    <Container>
+      <TextWrapper>
+        국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
+      </TextWrapper>
+      <Blue />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #cae9ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+  height: 100vh;
+`;
+
+const TextWrapper = styled.div`
+  width: 670px;
+  font-size 34px;
+  font-weight 700;
+  height 108.781px;
+  letter-spacing -0.612px;
+  line-height 54.4px;
+  margin-bottom 20px;
+  text-align center;
+`;
 
 export default Search;
